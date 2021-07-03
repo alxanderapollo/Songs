16e48583b9b02ff88db99d2935f1e8d3c3c4734c
Alex Salazar <alxanderapollo@gmail.com>
2021-07-03 13:58:46 -0400

    pCreated a Connect component passing props to SongList

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,7 +1,16 @@
+
+//1. impory connect
+//2. then the component from react
+//3.map state to props will hold the kind of object we want to pass into this component
+//4. impory connect and pass map state 
 import React from 'react';
 //we want to create an instance of the connect component 
 //so that we can pass the song list down from the provider and
 //print it out
+//this always the same number of steps 
+//call connect
+//create map state to props
+//pass it in the two functions
 import {connect} from 'react-redux';
 
 //this syntax calls on the react component lib
@@ -10,8 +19,19 @@ class SongList extends React.Component {
     render(){
         return <div>SongList</div>
     }
+}
+//gets called with all of the state inside of our redux store 
+//which will be our entire list of songs from the song reducer
+//and currently selected songs from the song reducer
 
+//songs will passed up as props to the connect component
+//which will then be passed to SongsList
+const mapStateToProps = (state) => {
+    console.log(this.props)
+   return{songs: state.songs}
 }
 //syntax: the first set connect() returns a function
 //the second set connect(SongList) invokes the function
-export default connect()(SongList);
+//mapStateToProps gets passed as the first argument down here
+//which then SongList recives as props
+export default connect(mapStateToProps)(SongList);
