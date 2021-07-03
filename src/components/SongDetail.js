a044420e6a989181d42693c3c3424194ada6269b
Alex Salazar <alxanderapollo@gmail.com>
2021-07-03 17:11:52 -0400

    Final commit, added Song Details UI which display song title and details

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -2,13 +2,25 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
-const SongDetail = (props) => {
-    console.log(props)
-    return <div>Song Detail</div>;
+//instead of sending the whole props we can destructure the props
+const SongDetail = ({song}) => {
+    //if the user hasent chosen a song yet prompt the user to do choose a song
+    if(!song) return <div>Select a Song</div>
+    // returns the song title onto the screen
+    return( <div>
+        <h2>Details for:</h2>
+        <p>
+            <b>Title: {song.title}</b>
+            <br/>
+            duration: {song.duration}
+        </p>
+        </div>
+    );
 };
 
 //want to connect this function to the provider 
 //called with out state object
+//the key that shows up on the props object can be changed here 
 const mapsStateToProp = (state) => {
     return{song: state.selectedSong}
 
